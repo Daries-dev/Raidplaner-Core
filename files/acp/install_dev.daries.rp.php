d39229009bf94c8ccc3c1acce5d7d034ccb88d6e
xDeraiser <deraiser@gmx.de>
2024-04-26 13:04:50 +0200

    opiton rp_default_game_id added

--- files/acp/install_dev.daries.rp.php
+++ files/acp/install_dev.daries.rp.php
@@ -1,6 +1,7 @@
 <?php
 
 use wcf\data\language\item\LanguageItemEditor;
+use wcf\data\option\OptionEditor;
 use wcf\data\package\PackageCache;
 use wcf\system\language\LanguageFactory;
 use wcf\system\WCF;
@@ -15,6 +16,7 @@ $statement->execute([
     'rp.game.dev.daries.rp.game.default',
     PackageCache::getInstance()->getPackageByIdentifier('dev.daries.rp')->packageID,
 ]);
+$gameID = WCF::getDB()->getInsertID('rp1_game', 'gameID');
 
 $titles = [
     'de' => 'Standard',
@@ -33,3 +35,14 @@ foreach ($titles as $languageCode => $value) {
         'packageID' => PackageCache::getInstance()->getPackageByIdentifier('dev.daries.rp')->packageID,
     ]);
 }
+
+// set default game to option
+$sql = "UPDATE  wcf1_option
+        SET     optionValue = ?
+        WHERE   optionName = ?";
+$statement = WCF::getDB()->prepare($sql);
+$statement->execute([
+    $gameID,
+    'rp_default_game_id',
+]);
+OptionEditor::resetCache();
