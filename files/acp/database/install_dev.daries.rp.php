ae80259213753c418a3e316c580d7147909c95d7
xDeraiser <deraiser@gmx.de>
2024-04-26 11:35:00 +0200

    database added

--- files/acp/database/install_dev.daries.rp.php
+++ files/acp/database/install_dev.daries.rp.php
@@ -6,15 +6,56 @@
  * @license Free License <https://daries.dev/en/license-for-free-plugins>
  */
 
+use wcf\system\database\table\column\DefaultFalseBooleanDatabaseTableColumn;
+use wcf\system\database\table\column\IntDatabaseTableColumn;
+use wcf\system\database\table\column\MediumtextDatabaseTableColumn;
 use wcf\system\database\table\column\NotNullInt10DatabaseTableColumn;
+use wcf\system\database\table\column\NotNullVarchar191DatabaseTableColumn;
 use wcf\system\database\table\column\NotNullVarchar255DatabaseTableColumn;
 use wcf\system\database\table\column\ObjectIdDatabaseTableColumn;
+use wcf\system\database\table\column\TextDatabaseTableColumn;
 use wcf\system\database\table\DatabaseTable;
 use wcf\system\database\table\index\DatabaseTableForeignKey;
 use wcf\system\database\table\index\DatabaseTableIndex;
 use wcf\system\database\table\index\DatabaseTablePrimaryIndex;
+use wcf\system\database\table\PartialDatabaseTable;
 
 return [
+    DatabaseTable::create('rp1_character')
+        ->columns([
+            ObjectIdDatabaseTableColumn::create('characterID'),
+            NotNullVarchar191DatabaseTableColumn::create('characterName'),
+            IntDatabaseTableColumn::create('userID')
+                ->length(10),
+            NotNullInt10DatabaseTableColumn::create('gameID'),
+            NotNullInt10DatabaseTableColumn::create('created')
+                ->defaultValue(0),
+            NotNullInt10DatabaseTableColumn::create('lastUpdateTime')
+                ->defaultValue(0),
+            MediumtextDatabaseTableColumn::create('notes'),
+            TextDatabaseTableColumn::create('additionalData'),
+            NotNullVarchar255DatabaseTableColumn::create('guildName')
+                ->defaultValue(''),
+            NotNullInt10DatabaseTableColumn::create('views')
+                ->defaultValue(0),
+            DefaultFalseBooleanDatabaseTableColumn::create('isPrimary'),
+            DefaultFalseBooleanDatabaseTableColumn::create('isDisabled'),
+        ])
+        ->indices([
+            DatabaseTablePrimaryIndex::create()
+                ->columns(['characterID']),
+            DatabaseTableIndex::create('characterName_gameID')
+                ->columns(['characterName', 'gameID'])
+                ->type(DatabaseTableIndex::UNIQUE_TYPE),
+        ])
+        ->foreignKeys([
+            DatabaseTableForeignKey::create()
+                ->columns(['userID'])
+                ->referencedTable('wcf1_user')
+                ->referencedColumns(['userID'])
+                ->onDelete('SET NULL'),
+        ]),
+
     DatabaseTable::create('rp1_game')
         ->columns([
             ObjectIdDatabaseTableColumn::create('gameID'),
@@ -36,4 +77,13 @@ return [
                 ->referencedColumns(['packageID'])
                 ->onDelete('CASCADE'),
         ]),
+
+    PartialDatabaseTable::create('rp1_character')
+        ->foreignKeys([
+            DatabaseTableForeignKey::create()
+                ->columns(['gameID'])
+                ->referencedTable('rp1_game')
+                ->referencedColumns(['gameID'])
+                ->onDelete('CASCADE'),
+        ])
 ];
