dde802cf5e43203f5013b1dbe8a4a3950f22aa91
xDeraiser <github@daries.dev>
2024-07-29 21:43:40 +0200

    Removed code for guest button label modification in add participant action class, as AJAX requests cannot modify the form via PHP.

--- files/lib/action/AddParticipantAction.class.php
+++ files/lib/action/AddParticipantAction.class.php
@@ -160,10 +160,6 @@ final class AddParticipantAction implements RequestHandlerInterface
 
         $form->build();
 
-        if (!WCF::getUser()->userID) {
-            $form->getButton('submitButton')->label('rp.event.raid.attendee.button.guest');
-        }
-
         return $form;
     }
 
